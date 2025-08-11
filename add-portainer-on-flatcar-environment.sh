f3d747c483cffbe99bfbbd012c9ad8ee4f15ca5d
Luis Alcaraz <me@lalcaraz.com>
2025-08-11 11:44:10 -0400

    Update add-portainer-on-flatcar-environment.sh

--- add-portainer-on-flatcar-environment.sh
+++ add-portainer-on-flatcar-environment.sh
@@ -1,1 +1,2 @@
-
+#!/bin/bash
+mkdir portainer; docker run -d -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v ./portainer:/data portainer/portainer-ce:lts
