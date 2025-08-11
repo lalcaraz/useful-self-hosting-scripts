c629529821a68c69517454489c24534f5a62d211
Luis Alcaraz <me@lalcaraz.com>
2025-08-11 11:45:25 -0400

    Update create-flatcar-proxmox-vm.sh

--- create-flatcar-proxmox-vm.sh
+++ create-flatcar-proxmox-vm.sh
@@ -13,23 +13,23 @@ else
 fi
 
 if [[ -n "$VM_ID" && -n "$STORAGE_ID" ]]; then
-        # Get Stable image
-        wget https://stable.release.flatcar-linux.net/amd64-usr/current/flatcar_production_proxmoxve_image.img
-
-        # create the vm and import the image to its disk
-        qm create $VM_ID --cores 2 --memory 4096 --net0 "virtio,bridge=vmbr0" --ipconfig0 "ip=dhcp"
-        qm disk import $VM_ID flatcar_production_proxmoxve_image.img $STORAGE_ID
-
-        # tell the vm to boot from the imported image
-        qm set $VM_ID --scsi0 $STORAGE_ID:vm-$VM_ID-disk-0
-        qm set $VM_ID --boot order=scsi0
-
-        # Create the cloud-init CD-ROM drive which activates the cloud-init options for the VM.
-        # This is required for using ignition config as well.
-        qm set $VM_ID --ide2 $STORAGE_ID:cloudinit
-
-        # Remove image after usage
-        rm flatcar_production_proxmoxve_image.img
+	# Get Stable image
+	wget https://stable.release.flatcar-linux.net/amd64-usr/current/flatcar_production_proxmoxve_image.img
+	
+	# create the vm and import the image to its disk
+	qm create $VM_ID --cores 2 --memory 4096 --net0 "virtio,bridge=vmbr0" --ipconfig0 "ip=dhcp"
+	qm disk import $VM_ID flatcar_production_proxmoxve_image.img $STORAGE_ID
+	
+	# tell the vm to boot from the imported image
+	qm set $VM_ID --scsi0 $STORAGE_ID:vm-$VM_ID-disk-0
+	qm set $VM_ID --boot order=scsi0
+	
+	# Create the cloud-init CD-ROM drive which activates the cloud-init options for the VM.
+	# This is required for using ignition config as well.
+	qm set $VM_ID --ide2 $STORAGE_ID:cloudinit
+	
+	# Remove image after usage
+	rm flatcar_production_proxmoxve_image.img
 else
 	echo ""
 	if [[ -z "$VM_ID" ]]; then
