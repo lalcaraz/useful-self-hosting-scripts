308287b2555f44e18aa314f692acf48a0e065161
Luis Alcaraz <me@lalcaraz.com>
2025-08-11 14:43:56 -0400

    Update create-flatcar-proxmox-vm.sh

--- create-flatcar-proxmox-vm.sh
+++ create-flatcar-proxmox-vm.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+IGNITION_CONFIG=$(cat <<EOF
+{
+  "ignition": { "version": "3.0.0" },
+  "systemd": {
+	"units": [
+	  {
+		"name": "docker.service",
+		"enabled": true
+	  }
+	]
+  },
+  "storage": {
+	"links": [
+	  {
+		"path": "/etc/systemd/system/multi-user.target.wants/docker.service",
+		"target": "/usr/lib/systemd/system/docker.service",
+		"hard": false,
+		"overwrite": true
+	  }
+	]
+  }
+}
+EOF
+)
+
 if [[ -n "$1" ]]; then
 	VM_ID=$1
 else
@@ -13,30 +38,38 @@ else
 fi
 
 if [[ -n "$VM_ID" && -n "$STORAGE_ID" ]]; then
-	# Get Stable image
-	wget https://stable.release.flatcar-linux.net/amd64-usr/current/flatcar_production_proxmoxve_image.img
-	
-	# create the vm and import the image to its disk
-	qm create $VM_ID --cores 2 --memory 4096 --net0 "virtio,bridge=vmbr0" --ipconfig0 "ip=dhcp"
-	qm disk import $VM_ID flatcar_production_proxmoxve_image.img $STORAGE_ID
-	
-	# tell the vm to boot from the imported image
-	qm set $VM_ID --scsi0 $STORAGE_ID:vm-$VM_ID-disk-0
-	qm set $VM_ID --boot order=scsi0
-	
-	# Create the cloud-init CD-ROM drive which activates the cloud-init options for the VM.
-	# This is required for using ignition config as well.
-	qm set $VM_ID --ide2 $STORAGE_ID:cloudinit
-	
-	# Remove image after usage
-	rm flatcar_production_proxmoxve_image.img
+        # Get Stable image
+        wget https://stable.release.flatcar-linux.net/amd64-usr/current/flatcar_production_proxmoxve_image.img
+
+        # create the vm and import the image to its disk
+        qm create $VM_ID --cores 2 --memory 4096 --net0 "virtio,bridge=vmbr0" --ipconfig0 "ip=dhcp"
+        qm disk import $VM_ID flatcar_production_proxmoxve_image.img $STORAGE_ID
+
+        # tell the vm to boot from the imported image
+        qm set $VM_ID --scsi0 $STORAGE_ID:vm-$VM_ID-disk-0
+        qm set $VM_ID --boot order=scsi0
+
+        # Create the cloud-init CD-ROM drive which activates the cloud-init options for the VM.
+        # This is required for using ignition config as well.
+        qm set $VM_ID --ide2 $STORAGE_ID:cloudinit
+
+		# Creates directory for user-data snippets for ignition config
+		mkdir -p /var/lib/vz/snippets/
+		rm -f /var/lib/vz/snippets/flatcar-user-data-$VM_ID 2>/dev/null
+		echo "$IGNITION_CONFIG" > /var/lib/vz/snippets/flatcar-user-data-$VM_ID
+
+		# Set the user-data snippet for ignition config
+		qm set $VM_ID --cicustom "user=local:snippets/flatcar-user-data-$VM_ID"
+
+        # Remove image after usage
+        rm flatcar_production_proxmoxve_image.img
 else
 	echo ""
-	if [[ -z "$VM_ID" ]]; then
-			echo "Error: VM ID is required as the first argument or via prompt."
-	fi
-	if [[ -z "$STORAGE_ID" ]]; then
-			echo "Error: Storage ID is required as the second argument or via prompt."
-	fi
-	exit 1
+        if [[ -z "$VM_ID" ]]; then
+                echo "Error: VM ID is required as the first argument or via prompt."
+        fi
+        if [[ -z "$STORAGE_ID" ]]; then
+                echo "Error: Storage ID is required as the second argument or via prompt."
+        fi
+        exit 1
 fi
